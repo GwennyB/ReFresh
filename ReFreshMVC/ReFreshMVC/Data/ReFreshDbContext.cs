e4ca589f8503b62598d8408544be331aa18bd888
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-02-19 11:00:41 -0800

    added empty DbContext

---
+++ ReFreshMVC/ReFreshMVC/Data/ReFreshDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReFreshMVC.Data
+{
+    public class ReFreshDbContext : DbContext
+    {
+
+        public ReFreshDbContext(DbContextOptions<ReFreshDbContext> options) : base(options)
+        {
+
+        }
+
+
+        /// <summary>
+        /// overrides (DbContext virtual) method that builds out basic API structure
+        /// maps composite keys
+        /// </summary>
+        /// <param name="modelBuilder">  </param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // DB seed data
+            //modelBuilder.Entity<___>().HasData();
+
+        }
+
+
+        //public DbSet<___> Posts { get; set; }
+
+    }
+}
+}
