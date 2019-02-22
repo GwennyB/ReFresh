b4750d8c1dce43cb37c54c35f6438ae0cfc7726a
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-02-22 14:26:19 -0800

    added Carnivore policy

---
+++ ReFreshMVC/ReFreshMVC/Models/Handler/DietRestriction.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReFreshMVC.Models.Handler
+{
+    public class DietRestriction : AuthorizationHandler<DietRestriction>, IAuthorizationRequirement
+    {
+
+        /// <summary>
+        /// establishes product access restrictions based on user's dietary preferences
+        /// </summary>
+        /// <param name="context"> authorization handler context </param>
+        /// <param name="requirement"> dietary restriction requirement </param>
+        /// <returns></returns>
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, DietRestriction requirement)
+        {
+            string carnivore = context.User.Claims.FirstOrDefault(c => c.Type == "Carnivore").Value;
+
+            if (carnivore == "true")
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+
+        }
+    }
+
+}
