743831337f538b13ee98024289bdacc236bf2011
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-02-26 11:48:40 -0800

    added Cart and Order models

---
+++ ReFreshMVC/ReFreshMVC/Models/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReFreshMVC.Models
+{
+    public class Order
+    {
+        [Required]
+        public int CartID { get; set; }
+
+        [Required]
+        public int ProductID { get; set; }
+
+        [Required]
+        [Display(Name = "Quantity")]
+        [DataType(DataType.Currency)]
+        public int Qty { get; set; }
+
+        [Required]
+        [Display(Name = "Total Price")]
+        [DataType(DataType.Currency)]
+        public int ExtPrice { get; set; }
+
+
+        // Navigation Properties
+        public Cart Cart { get; set; }
+        public Product Product { get; set; }
+
+    }
+}
