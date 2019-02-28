cae57515a28ef553050a1793bf9962f600f645c1
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-02-27 20:51:58 -0800

    adding OAuth to UserController

---
+++ ReFreshMVC/ReFreshMVC/Models/ViewModels/ExtLoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReFreshMVC.Models.ViewModels
+{
+    public class ExtLoginViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+    }
+}
