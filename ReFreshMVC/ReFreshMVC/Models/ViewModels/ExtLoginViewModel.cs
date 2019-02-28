7fd9b5beb03e6becc4408c9f0b7cbaad5f2e369b
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-02-27 20:57:12 -0800

    added ExtLogin and ExtLoginViewModel (based on Register)

--- ReFreshMVC/ReFreshMVC/Models/ViewModels/ExtLoginViewModel.cs
+++ ReFreshMVC/ReFreshMVC/Models/ViewModels/ExtLoginViewModel.cs
@@ -1,12 +1,43 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ReFreshMVC.Models.ViewModels
 {
     public class ExtLoginViewModel
     {
         [Required]
-        [DataType(DataType.EmailAddress)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Birthdate")]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+
+        [Required]
+        [Display(Name = "E-Mail Address")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match. Please try again.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Do you eat meat?")]
+        public string EatsMeat { get; set; }
 
     }
 }
+
+
